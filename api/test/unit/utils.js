9a6e9e6d9231dff7cae6805f53afd28abb35db66
Christian Fei <crifei93@gmail.com>
2015-05-20 22:04:51 +0200

    removed some more ; because its sooo much fun

--- api/test/unit/utils.js
+++ api/test/unit/utils.js
@@ -1,11 +1,11 @@
-var utils = require('../../modules/utils');
-var expect = require('chai').expect;
+var utils = require('../../modules/utils')
+var expect = require('chai').expect
 
 describe('utils', function() {
   it('should remove unnecessary fields from a pomodoro', function () {
-    var cleanedPomodoro = utils.cleanPomodoro({'should_be_removed':true,'startedAt':123456789});
-    expect(cleanedPomodoro).to.have.property('startedAt');
-    expect(cleanedPomodoro).not.to.have.property('should_be_removed');
-  });
+    var cleanedPomodoro = utils.cleanPomodoro({'should_be_removed':true,'startedAt':123456789})
+    expect(cleanedPomodoro).to.have.property('startedAt')
+    expect(cleanedPomodoro).not.to.have.property('should_be_removed')
+  })
 
-});
+})
