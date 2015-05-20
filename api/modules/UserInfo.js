7326367079b59e39a9d88c32ec5d1b3fee1e71b6
Christian Fei <crifei93@gmail.com>
2015-05-20 22:03:43 +0200

    removed some more ;

--- api/modules/UserInfo.js
+++ api/modules/UserInfo.js
@@ -1,29 +1,29 @@
 module.exports = function UserInfo(raw){
-  var self = this;
+  var self = this
 
-  self.apikey = require('crypto').randomBytes(20).toString('hex');
+  self.apikey = require('crypto').randomBytes(20).toString('hex')
 
-  self.username = self.avatar = self.id = null;
+  self.username = self.avatar = self.id = null
 
-  self.id = raw.id;
+  self.id = raw.id
   switch(raw.provider){
     case 'twitter':
-      self.username = raw.username;
-      self.avatar = raw._json.profile_image_url;
-      break;
+      self.username = raw.username
+      self.avatar = raw._json.profile_image_url
+      break
     case 'github':
-      self.username = raw.username;
-      self.avatar = raw._json.avatar_url;
-      break;
+      self.username = raw.username
+      self.avatar = raw._json.avatar_url
+      break
     default:
   }
 
   self.toJSON = function(){
-    var replacement = new Object();
+    var replacement = new Object()
     for (var prop in self) {
       if( typeof prop !== 'function')
-        replacement[prop] = self[prop];
+        replacement[prop] = self[prop]
     }
-    return replacement;
-  };
-};
+    return replacement
+  }
+}
