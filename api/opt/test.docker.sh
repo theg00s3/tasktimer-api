4245db8dab29f55398370921c48d84c639b91a27
Christian Fei <christian.fei@xpeppers.com>
2015-07-11 17:32:34 +0200

    updated api test script

--- api/opt/test.docker.sh
+++ api/opt/test.docker.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it \
-  --link=pomodoro-api-db-test:pomodoro-api-db \
-  --link pomodoro-api-sessions:pomodoro-api-sessions \
-  -v /vagrant/credentials.json:/credentials.json \
-  -v /vagrant/api:/app \
-  christianfei/pomodoro-api \
-  npm test
+id_for_container(){
+  CONTAINER="$1\s*$"
+  CONTAINER_ID="$(docker ps -a | grep "$CONTAINER" | awk '{print $1}')"
+  echo $CONTAINER_ID
+}
+
+if [ -z "$(id_for_container 'pomodoro-api-db-test')" ]; then
+  docker run --name pomodoro-api-db-test --restart=always -d mongo:latest
+
+fi
+
+docker run -it --link=pomodoro-api-db-test:pomodoro-api-db -v /pomodoro.cc/credentials.json:/credentials.json christianfei/pomodoro-api sh -c 'npm install && npm test'
