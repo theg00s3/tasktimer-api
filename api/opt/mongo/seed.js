37d6db47735565e8f190c66962e8037d4a43f5a6
Christian Fei <christian.fei@xpeppers.com>
2015-07-12 21:04:32 +0200

    added mongo.seed

---
+++ api/opt/mongo/seed.js
@@ -0,0 +1,71 @@
+var userId = 2662706
+
+use pomodoro;
+
+db.pomodori.remove({
+  "startedAt": {
+    $gte: new Date("2015-07-12T00:00:00.000+02:00"),
+    $lt: new Date("2015-07-13T00:00:00.000+02:00"),
+  }
+})
+
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T09:00:00.000+02:00"),
+  "minutes": 25,
+  "type": "pomodoro",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T09:25:00.000+02:00"),
+  "minutes": 5,
+  "type": "break",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T09:30:00.000+02:00"),
+  "minutes": 25,
+  "type": "pomodoro",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T09:55:00.000+02:00"),
+  "minutes": 5,
+  "type": "break",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T10:00:00.000+02:00"),
+  "minutes": 25,
+  "type": "pomodoro",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T10:25:00.000+02:00"),
+  "minutes": 5,
+  "type": "break",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T10:30:00.000+02:00"),
+  "minutes": 25,
+  "type": "pomodoro",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T10:55:00.000+02:00"),
+  "minutes": 15,
+  "type": "break",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T11:30:00.000+02:00"),
+  "minutes": 25,
+  "type": "pomodoro",
+  "userId": userId
+})
+db.pomodori.insert({
+  "startedAt": new Date("2015-07-12T12:34:00.000+02:00"),
+  "minutes": 25,
+  "type": "pomodoro",
+  "userId": userId
+})
