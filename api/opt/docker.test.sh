bd4030898244b7a8f4b309e7426887ae38efb4ab
Christian Fei <crifei93@gmail.com>
2015-07-21 21:41:38 +0200

    running api tests inside docker, with mongo and redis

--- api/opt/docker.test.sh
+++ api/opt/docker.test.sh
@@ -9,6 +9,15 @@ id_for_container(){
 if [ -z "$(id_for_container 'pomodoro-api-db-test')" ]; then
   docker run --name pomodoro-api-db-test -d mongo:latest
 fi
+if [ -z "$(id_for_container 'pomodoro-api-sessions-test')" ]; then
+  docker run --name pomodoro-api-sessions-test -d redis:latest redis-server
+fi
 
-docker run --rm -it --link=pomodoro-api-db-test:pomodoro-api-db --env ENV="DEV" -v /pomodoro.cc/credentials.json:/credentials.json -v /pomodoro.cc/api:/app christianfei/pomodoro-api sh -c 'npm install && npm test'
+docker run --rm -it \
+  --link=pomodoro-api-db-test:pomodoro-api-db \
+  --link=pomodoro-api-sessions-test:pomodoro-api-sessions \
+  --env ENV="DEV" \
+  -v /pomodoro.cc/credentials.json:/credentials.json \
+  -v /pomodoro.cc/api:/app \
+  christianfei/pomodoro-api sh -c 'npm install && npm test'
 docker rm -f pomodoro-api-db-test
