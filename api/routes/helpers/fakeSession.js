2dcd197fde28aee54160db3752b60ef77166a8b7
Christian Fei <crifei93@gmail.com>
2015-08-13 10:43:13 +0200

    extracted fakeUser variable in fakeSession

--- api/routes/helpers/fakeSession.js
+++ api/routes/helpers/fakeSession.js
@@ -3,7 +3,7 @@ module.exports = function(router){
   router.get('/auth/fake', fakeSessionRoute)
 }
 
-
+var fakeUser = {"apikey":"fake","id":2662706,"avatar":"https://avatars.githubusercontent.com/u/2662706?v=3","username":"christian-fei","_id":"fake"}
 
 function fakeSessionHandler(req,res,next){
   if( req && req.session && req.session.user_tmp ){
@@ -16,6 +16,6 @@ function fakeSessionHandler(req,res,next){
 
 function fakeSessionRoute(req,res){
   if( !req.session ) { req.session = {} }
-  req.session.user_tmp = {"apikey":"fake","id":2662706,"avatar":"https://avatars.githubusercontent.com/u/2662706?v=3","username":"christian-fei","_id":"fake"}
+  req.session.user_tmp = fakeUser
   res.redirect('/')
 }
