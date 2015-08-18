650478617901bfe199851de4765506b7c533e20b
Christian Fei <crifei93@gmail.com>
2015-08-19 00:40:13 +0200

    refactoring /auth/fake route

--- api/routes/helpers/fakeSession.js
+++ api/routes/helpers/fakeSession.js
@@ -1,21 +1,24 @@
-module.exports = function(router){
-  router.use(fakeSessionHandler)
-  router.get('/auth/fake', fakeSessionRoute)
-}
+var router = require('express').Router()
+
+router.use(fakeSessionHandler)
+router.get('/fake', fakeAuth)
+
+module.exports = router
+
+
+
 
 var fakeUser = {"apikey":"fake","id":2662706,"avatar":"https://avatars.githubusercontent.com/u/2662706?v=3","username":"christian-fei","_id":"fake"}
 
 function fakeSessionHandler(req,res,next){
-  if( req && req.session && req.session.user_tmp ){
-    req.user = req.session.user_tmp
+  if( req && req.session && req.session.userTmp ){
+    req.user = req.session.userTmp
   }
-  if( next ){
-    next()
-  }
+  next()
 }
 
-function fakeSessionRoute(req,res){
+function fakeAuth(req,res){
   if( !req.session ) { req.session = {} }
-  req.session.user_tmp = fakeUser
+  req.session.userTmp = fakeUser
   res.redirect('/')
 }
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
 var router = require('express').Router()
 
-
 if( process.env.ENV==='DEV' || process.env.NODE_ENV==='test' ){
-  require('./helpers/fakeSession')(router)
+  router.use('/auth', require('./helpers/fakeSession'))
 }
 
-module.exports = router
+router
   .use('/api', require('./api'))
   .use('/auth', require('./auth'))
+
+
+module.exports = router
