593ad294fee0f2d5e1eb3af7faffc12db85f918c
Christian Fei <crifei93@gmail.com>
2015-08-19 14:44:57 +0200

    Revert "refactoring /auth/fake route"
    This reverts commit 650478617901bfe199851de4765506b7c533e20b.

--- api/routes/helpers/fakeSession.js
+++ api/routes/helpers/fakeSession.js
@@ -1,24 +1,21 @@
-var router = require('express').Router()
-
-router.use(fakeSessionHandler)
-router.get('/fake', fakeAuth)
-
-module.exports = router
-
-
-
+module.exports = function(router){
+  router.use(fakeSessionHandler)
+  router.get('/auth/fake', fakeSessionRoute)
+}
 
 var fakeUser = {"apikey":"fake","id":2662706,"avatar":"https://avatars.githubusercontent.com/u/2662706?v=3","username":"christian-fei","_id":"fake"}
 
 function fakeSessionHandler(req,res,next){
-  if( req && req.session && req.session.userTmp ){
-    req.user = req.session.userTmp
+  if( req && req.session && req.session.user_tmp ){
+    req.user = req.session.user_tmp
   }
-  next()
+  if( next ){
+    next()
+  }
 }
 
-function fakeAuth(req,res){
+function fakeSessionRoute(req,res){
   if( !req.session ) { req.session = {} }
-  req.session.userTmp = fakeUser
+  req.session.user_tmp = fakeUser
   res.redirect('/')
 }
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,10 @@
 var router = require('express').Router()
 
+
 if( process.env.ENV==='DEV' || process.env.NODE_ENV==='test' ){
-  router.use('/auth', require('./helpers/fakeSession'))
+  require('./helpers/fakeSession')(router)
 }
 
-router
+module.exports = router
   .use('/api', require('./api'))
   .use('/auth', require('./auth'))
-
-
-module.exports = router
