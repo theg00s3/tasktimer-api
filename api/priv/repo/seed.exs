4e16386f8155bb74a403a8ce3c78092d9d69983a
Christian Fei <crifei93@gmail.com>
2015-12-26 17:07:35 +0100

    rm blog

---
+++ api/priv/repo/seed.exs
@@ -0,0 +1,23 @@
+alias Api.Models.Pomodoro
+
+defmodule TimeHelpers do
+  def datetime_for(hour) do
+    {:ok, time} = Ecto.Time.cast(hour)
+    Ecto.DateTime.from_date_and_time(Ecto.Date.local, time)
+  end
+end
+
+user_id  = 2662706
+
+pomodoros = [
+  %Pomodoro{
+    type: "pomodoro",
+    minutes: 25,
+    started_at: TimeHelpers.datetime_for("09:00:00Z")
+  }
+]
+
+Enum.each(pomodoros, fn(p) ->
+  Api.Repo.create_pomodoro_for(user_id, p)
+end)
+
