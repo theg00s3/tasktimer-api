7c2f54db815abf08647ed1fc20d65d96ab87c02a
Christian Fei <crifei93@gmail.com>
2016-02-15 23:35:00 +0100

    added integration tests for creating a pomodoro and todo

--- api/test/api_v2/router_test.exs
+++ api/test/api_v2/router_test.exs
@@ -2,6 +2,8 @@ defmodule Api.Router.Test do
   use ExUnit.Case, async: true
   use Plug.Test
 
+  @pomodoro %{type: "pomodoro", minutes: 25, started_at: Ecto.DateTime.utc}
+
   test "authenticates requests" do
     conn = conn(:get, "/api/pomodoros")
              |> put_req_header("cookie", "invalid")
@@ -16,12 +18,24 @@ defmodule Api.Router.Test do
     assert conn.status == 200
   end
 
-  @tag :skip
   test "creates pomodoro" do
-    conn = conn(:post, "/api/pomodoros", "{\"type\":\"pomodoro\", \"minutes\": 25}")
+    conn = conn(:post, "/api/pomodoros", @pomodoro)
+             |> put_req_header("content-type", "application/json")
              |> put_req_header("cookie", "authorized")
              |> Api.Router.call([])
 
-    assert conn.status == 200
+    assert conn.status == 201
+  end
+
+  test "creates todo" do
+    conn = conn(:post, "/api/todos", %{text: "just a todo", completed: false})
+             |> put_req_header("content-type", "application/json")
+             |> put_req_header("cookie", "authorized")
+             |> Api.Router.call([])
+
+    assert conn.status == 201
+  end
+
+  test "associates pomodoro to todo" do
   end
 end
