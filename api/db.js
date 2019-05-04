c0aaaf330401f298a197f6fd0e7b38648771e25b
Christian Fei <crifei93@gmail.com>
2019-05-04 12:18:14 +0200

    log NODE_ENV

--- api/db.js
+++ api/db.js
@@ -3,4 +3,5 @@ if (!process.env.NOW_REGION) { require('dotenv').config() }
 const monk = require('monk')
 
 // console.log('process.env.MONGO_URL', process.env.MONGO_URL)
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 module.exports = monk(process.env.MONGO_URL)
