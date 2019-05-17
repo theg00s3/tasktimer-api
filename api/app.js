7ce4ff9d64c261f0be7cd44db8c51e3e9b8d3179
Christian Fei <crifei93@gmail.com>
2019-05-17 23:59:20 +0200

    add sentry to api

--- api/app.js
+++ api/app.js
@@ -1,4 +1,7 @@
 const app = require('express')()
+const Sentry = require('@sentry/node')
+Sentry.init({ dsn: process.env.SENTRY_DSN })
+
 const middlewares = require('./middlewares')
 
 console.log('MONGO_URL set?', !!process.env.MONGO_URL)
