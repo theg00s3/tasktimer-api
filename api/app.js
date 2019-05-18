bb223ba55f5907fd5a4eff25d37f88da8b13be9f
Christian Fei <crifei93@gmail.com>
2019-05-18 09:30:56 +0200

    log

--- api/app.js
+++ api/app.js
@@ -4,8 +4,6 @@ Sentry.init({ dsn: process.env.SENTRY_DSN })
 
 const middlewares = require('./middlewares')
 
-console.log('MONGO_URL set?', !!process.env.MONGO_URL)
-
 app.set('trust proxy', 1)
 app.use(...middlewares)
 
--- api/db.js
+++ api/db.js
@@ -1,4 +1,5 @@
 const monk = require('monk')
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+console.log('MONGO_URL set?', !!process.env.MONGO_URL)
 module.exports = monk(process.env.MONGO_URL)
