872e8a5c64f90120c02baec6976dbaf986b472f9
Christian Fei <crifei93@gmail.com>
2019-06-05 20:14:41 +0200

    try fix create subscription

--- api/routes/api.js
+++ api/routes/api.js
@@ -53,8 +53,6 @@ api.post('/api/create-subscription', async function (req, res) {
 
   const user = await User.findOneAndUpdate({ _id: userId }, { $set: { updatedAt: new Date(), customer, customerUpdatedAt: new Date(), subscription, subscriptionUpdatedAt: new Date() } }, { new: true })
 
-  req.session = user
-
   return res.json({ message: 'create-subscription-succeeded', user })
 })
 
