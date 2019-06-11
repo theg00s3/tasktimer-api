e5e1290eabd31d703fcef728cdc3a11019d44ccd
Christian Fei <crifei93@gmail.com>
2019-06-11 20:11:01 +0200

    set log level api

--- api/routes/api.js
+++ api/routes/api.js
@@ -79,6 +79,7 @@ api.post('/pomodoros', async (req, res) => {
   logger.info('pomodoro, errors', pomodoro, errors)
 
   if (errors) {
+    logger.info('pomodoro validation errors', errors)
     await Event.insert({ name: 'pomodoroFailedValidation', createdAt: new Date(), userId: userId, pomodoro }).catch(Function.prototype)
     res.writeHead(422)
     return res.end()
@@ -86,6 +87,7 @@ api.post('/pomodoros', async (req, res) => {
 
   const duplicateCount = await Pomodoro.count({ userId, startedAt: pomodoro.startedAt })
   if (duplicateCount > 0) {
+    logger.info('found duplicate pomodoro', duplicateCount)
     await Event.insert({ name: 'pomodoroDuplicate', createdAt: new Date(), userId: userId, pomodoro }).catch(Function.prototype)
     res.writeHead(409)
     return res.end()
@@ -93,6 +95,7 @@ api.post('/pomodoros', async (req, res) => {
 
   Object.assign(pomodoro, { userId })
 
+  logger.info('inserting pomodoro', pomodoro)
   await Pomodoro.insert(pomodoro)
   await Event.insert({ name: 'pomodoroCreated', createdAt: new Date(), userId: userId, pomodoro }).catch(Function.prototype)
 
