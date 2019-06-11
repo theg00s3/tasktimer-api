ff791d0f794ff905a8d7a86d7c0fddd814b7ed70
Christian Fei <crifei93@gmail.com>
2019-06-11 20:40:28 +0200

    track userAuthenticated event

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,15 @@
 const api = require('../app')
+const Event = require('../models/Event')
 
 module.exports = api
 
-api.get('/user/info', (req, res) => {
+api.get('/user/info', async (req, res) => {
   if (!req.user) {
     res.writeHead(401)
     return res.end()
   }
   res.json(req.user)
+  await Event.insert({ name: 'userAuthenticated', createdAt: new Date(), user: { _id: req.user._id, username: req.user.username } }).catch(Function.prototype)
 })
 api.get('/user/logout', (req, res) => {
   req.logout()
