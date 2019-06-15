30c52806dea0582c5f8ddedec6daccf29bb69f95
Christian Fei <crifei93@gmail.com>
2019-06-15 17:43:43 +0200

    log id during migration

--- api/migrations/20190615152953-unify-user-id-on-pomodoros.js
+++ api/migrations/20190615152953-unify-user-id-on-pomodoros.js
@@ -4,6 +4,7 @@ module.exports = {
   async up (db) {
     const usersColl = db.collection('users')
     usersColl.find({}).forEach(function (doc) {
+      console.log('updating doc', doc._id)
       usersColl.updateOne({ _id: doc._id }, { $set: { 'userId': monk.id(doc.userId) } })
     })
   },
