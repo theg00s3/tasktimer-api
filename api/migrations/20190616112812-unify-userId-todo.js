14c119b84de456be1d06de804ad9e9716431e63e
Christian Fei <crifei93@gmail.com>
2019-06-17 20:23:02 +0200

    add migration for unifying userId on todos

--- api/migrations/20190616112812-unify-userId-todo.js
+++ api/migrations/20190616112812-unify-userId-todo.js
@@ -1,9 +1,10 @@
+const monk = require('monk')
 module.exports = {
   async up (db) {
     const todosColl = db.collection('todos')
-    todosColl.find({ user: { $exists: true } }).forEach(function (doc) {
+    todosColl.find({ userId: { $exists: true } }).forEach(function (doc) {
       console.log('updating doc', doc._id, doc.createdAt, new Date(doc.createdAt))
-      todosColl.updateOne({ _id: doc._id }, { $set: { userId: doc.user && doc.user._id }, $unset: { 'user': '' } })
+      todosColl.updateOne({ _id: doc._id }, { $set: { userId: monk.id(doc.userId) } })
     })
   },
 
