86af2e438b386be1f98449d91cf09a4b9c699086
Christian Fei <crifei93@gmail.com>
2019-06-18 19:26:16 +0200

    usse createdAt for todoEvent

--- api/modules/update-user-todo.js
+++ api/modules/update-user-todo.js
@@ -10,24 +10,18 @@ module.exports = {
 
 async function updateUserTodo ({ user, todo }) {
   const userId = user._id
-  await Event.insert({ name: 'updateTodo', updatedAt: new Date(), user, todo }).catch(Function.prototype)
+  await Event.insert({ name: 'updateTodo', createdAt: new Date(), user, todo }).catch(Function.prototype)
 
   const errors = todoValidationErrors(todo)
   logger.info('todo, errors', todo, errors)
   if (errors === null) {
     Object.assign(todo, { userId })
-    if (todo.completedAt) {
-      Object.assign(todo, { completedAt: new Date(todo.completedAt) })
-    }
-    if (todo.deletedAt) {
-      Object.assign(todo, { deletedAt: new Date(todo.deletedAt) })
-    }
     await Todo.update({ _id: todo._id }, { $set: todo })
 
-    await Event.insert({ name: 'todoUpdated', updatedAt: new Date(), user, todo }).catch(Function.prototype)
+    await Event.insert({ name: 'todoUpdated', createdAt: new Date(), user, todo }).catch(Function.prototype)
     return todo
   } else {
-    await Event.insert({ name: 'todoFailedValidation', updatedAt: new Date(), user, todo }).catch(Function.prototype)
+    await Event.insert({ name: 'todoFailedValidation', createdAt: new Date(), user, todo }).catch(Function.prototype)
     throw new ValidationError(errors)
   }
 }
