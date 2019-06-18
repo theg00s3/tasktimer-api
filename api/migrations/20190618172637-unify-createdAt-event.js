70225145634894edf3a071124300edf288635991
Christian Fei <crifei93@gmail.com>
2019-06-18 19:37:41 +0200

    migration for updatedAt -> createdAt events

---
+++ api/migrations/20190618172637-unify-createdAt-event.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async up (db) {
+    const eventsColl = db.collection('events')
+    eventsColl.find({ updatedAt: { $exists: true } }).forEach(function (doc) {
+      console.log('updating doc', doc._id, doc.name, doc.updatedAt, new Date(doc.updatedAt))
+      eventsColl.updateOne({ _id: doc._id }, { $set: { 'createdAt': new Date(doc.updatedAt) }, $unset: { 'updatedAt': true } })
+    })
+  },
+
+  async down (db) {
+  }
+}
