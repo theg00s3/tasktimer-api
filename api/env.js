e5e9f2f428a63b284c17896d7b9b2af774ddf9c4
Christian Fei <crifei93@gmail.com>
2019-06-21 09:35:27 +0200

    improve env

--- api/env.js
+++ api/env.js
@@ -1,6 +1,5 @@
 const path = require('path')
 
-const production = process.env.NODE_ENV === 'production'
-const endFileName = production ? '.env.production' : '.env.development'
+const endFileName = `.env${process.env.NODE_ENV && `.${process.env.NODE_ENV}`}`
 const pathToEnvFile = path.resolve(__dirname, endFileName)
 require('dotenv').config({ path: pathToEnvFile })
