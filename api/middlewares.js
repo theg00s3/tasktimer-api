ad4ec9403e1468b5d1e0281e1eb420e6735a8399
Christian Fei <crifei93@gmail.com>
2020-01-28 23:05:16 +0100

    cleanup

--- api/middlewares.js
+++ api/middlewares.js
@@ -6,7 +6,7 @@ const morgan = require('morgan')
 module.exports = [
   morgan(':status\t :method\t :response-time ms\t :date[clf]\t :url\t\t'),
   cors({
-    origin: true, // ['https://pomodoro.cc', 'http://localhost:1234', 'http://localhost:3000'], // true, // ['https://pomodoro.cc', 'http://beta.pomodoro.cc', 'https://beta.pomodoro.cc', 'https://app.pomodoro.cc', 'https://dev.pomodoro.cc', 'http://dev.pomodoro.cc', 'http://dev.pomodoro.cc:9000'],
+    origin: true,
     methods: ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['x-now-id', 'x-now-trace', 'x-powered-by', 'Origin', 'Accept', 'Content-Type', 'Set-Cookie'],
     credentials: true
