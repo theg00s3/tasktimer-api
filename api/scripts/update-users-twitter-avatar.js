39bc0651a2e2b9719c7cb407514f667349aeba43
Christian Fei <crifei93@gmail.com>
2020-02-06 22:23:53 +0100

    logs

--- api/scripts/update-users-twitter-avatar.js
+++ api/scripts/update-users-twitter-avatar.js
@@ -26,17 +26,20 @@ async function main () {
     '1.0A', null, 'HMAC-SHA1'
   )
 
+  const count = await User.count({ twitterAvatarNotFound: { $exists: false }, $or: [{ twitterAvatarUpdatedAt: { $lt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7) } }, { twitterAvatarUpdatedAt: { $exists: false } }] })
+  console.log('count', count)
+
   await User.find({ twitterAvatarNotFound: { $exists: false }, $or: [{ twitterAvatarUpdatedAt: { $lt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7) } }, { twitterAvatarUpdatedAt: { $exists: false } }] })
     .each(async (doc, { pause, resume }) => {
       pause()
       const { username } = doc
       if (!username) {
-        console.info('no username not found', doc._id)
+        console.info('❌  no username not found', doc._id)
         return resume()
       }
       const avatar = await getTwitterAvatarUrlByUsername(username).catch(() => {})
       if (!avatar) {
-        console.info('avatar not found', username)
+        console.info('⚠️  avatar not found', username)
         await User.findOneAndUpdate({ _id: doc._id }, { $set: { twitterAvatarNotFound: true } })
 
         return resume()
@@ -44,7 +47,7 @@ async function main () {
 
       console.log('username', username, avatar)
       await User.findOneAndUpdate({ _id: doc._id }, { $set: { avatar, twitterAvatarUpdatedAt: new Date() } })
-      console.log(' -> updated', username, avatar)
+      console.log('✅  -> updated', username, avatar)
       resume()
     })
     .catch(err => {
@@ -52,7 +55,6 @@ async function main () {
     })
 
   async function getTwitterAvatarUrlByUsername (username) {
-    console.log('getTwitterAvatarUrlByUsername username', username)
     return new Promise((resolve, reject) => {
       oauth.get(
         `https://api.twitter.com/1.1/users/show.json?screen_name=${username}`,
@@ -60,7 +62,7 @@ async function main () {
         process.env.TWITTER_ACCESS_TOKEN_SECRET,
         function (e, data, res) {
           if (e) {
-            console.error(e)
+            // console.error(e)
             return reject(e)
           }
           const json = JSON.parse(data)
