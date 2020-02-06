7489b4922ab3e06ffdfd034e853af795e0dbdab2
Christian Fei <crifei93@gmail.com>
2020-02-06 21:19:17 +0100

    scripts/update-users-twitter-avatar.js

---
+++ api/scripts/update-users-twitter-avatar.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+require('../env')
+console.log('process.env.MONGO_URL', process.env.MONGO_URL)
+const OAuth = require('oauth')
+const User = require('../models/User')
+
+if (require.main === module) {
+  main()
+    .then(res => {
+      console.log(res)
+      process.exit(0)
+    })
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
+}
+
+async function main () {
+  var oauth = new OAuth.OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    process.env.TWITTER_CONSUMER_KEY,
+    process.env.TWITTER_CONSUMER_SECRET,
+    '1.0A', null, 'HMAC-SHA1'
+  )
+
+  await User.find({ $or: [{ avatar: 'http://pbs.twimg.com/profile_images/1182350902474297344/X2Xmd8Gr_normal.jpg' }, { twitterAvatarUpdatedAt: { $lt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7) } }, { twitterAvatarUpdatedAt: { $exists: false } }] })
+    .each(async (doc, { pause, resume }) => {
+      pause()
+      const { username } = doc
+      if (!username) {
+        console.info('no username not found', doc._id)
+        return resume()
+      }
+      const avatar = await getTwitterAvatarUrlByUsername(username).catch(() => {})
+      if (!avatar) {
+        console.info('avatar not found', username)
+        return resume()
+      }
+
+      console.log('username', username, avatar)
+      await User.findOneAndUpdate({ _id: doc._id }, { $set: { avatar, twitterAvatarUpdatedAt: new Date() } })
+      console.log(' -> updated', username, avatar)
+      resume()
+    })
+    .catch(err => {
+      console.error(err)
+    })
+
+  async function getTwitterAvatarUrlByUsername (username) {
+    console.log('getTwitterAvatarUrlByUsername username', username)
+    return new Promise((resolve, reject) => {
+      oauth.get(
+        `https://api.twitter.com/1.1/users/show.json?screen_name=${username}`,
+        process.env.TWITTER_ACCESS_KEY,
+        process.env.TWITTER_ACCESS_TOKEN_SECRET,
+        function (e, data, res) {
+          if (e) {
+            console.error(e)
+            return reject(e)
+          }
+          const json = JSON.parse(data)
+          // console.log('tw response', json.profile_image_url)
+          resolve(json.profile_image_url)
+        })
+    })
+  }
+}
